4e650d196dc6343b45e8b3a6aeca930748546d4a
Naema <94893024+ranmori@users.noreply.github.com>
2025-10-30 21:45:53 +0300

    Implement numberGame function to rearrange numbers

---
+++ MinimumNumber.js
@@ -0,0 +1,10 @@
+
+
+
+var numberGame = function(nums) {
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length; i += 2) {
+        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+    }
+    return nums;
+};
