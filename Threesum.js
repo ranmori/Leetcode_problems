00a5521d065596328ae43983fcdafd29e83607f8
Naema <94893024+ranmori@users.noreply.github.com>
2025-11-16 14:00:05 +0300

    Add threeSum function to find triplets summing to zero
    Implemented the threeSum function to find all unique triplets in an array that sum to zero.

--- Threesum.js
+++ Threesum.js
@@ -31,3 +31,40 @@ var threeSum = function(nums) {
     }
     return res;
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    // sort them array
+     nums.sort((a,b)=>a-b)
+    let res=[]
+    for(let i=0; i<nums.length; i++){
+        // avoid duplicates 
+        if(i>0 && nums[i] ===nums[i-1]) continue
+        
+        let left= i+1
+        let right=nums.length-1
+
+        while(left<right){
+            let sum= nums[i] + nums[left] + nums[right]
+            if(sum===0){
+                res.push([nums[i],nums[left],nums[right]])
+                left++
+                right --
+
+                 // skip duplicates
+                while (left < right && nums[left] === nums[left - 1]) left++;
+                while (left < right && nums[right] === nums[right + 1]) right--;
+
+                    
+            }
+            else if (sum <0) left ++
+            else right--
+            
+        }
+    }
+    return res
+};
