709e3e3bba65f7637ddb38314434463a4160c570
Naema <94893024+ranmori@users.noreply.github.com>
2026-01-29 21:58:06 +0300

    Add mergeAlternately function
    Implement function to merge two strings alternately.

---
+++ merge.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let res=[]
+    let i=0;
+    let k=0
+     while(i<word1.length && k<word2.length){
+       res.push(word1[i++]+(word2[k++]))
+        
+     }
+    res.push(word1.slice(i))
+    res.push(word2.slice(k))
+    return res.join("");
+};
